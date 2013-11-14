85894fc5f1a557b8187663383c155c0fa2142a17
Mats Groenlid <groenlid@threepwood>
2013-11-14 21:41:33 +0100

    as

--- Uranime/uranime.py
+++ Uranime/uranime.py
@@ -1,16 +1,47 @@
 from xdm.plugins import *
 import datetime
-from lib import dateutil
+import xml.etree.ElementTree as ET
+from lib import requests
+import re
+from lib.dateutil.parser import parser as dateParser
+
+import uranime
 
 class Uranime(Provider){
     version = "0.1"
-    identifier = "de.lad1337.urani.me"
+    identifier = "de.lad1337.uranime"
     _tag = 'uranime'
     _additional_tags = ['anime']
     single = True
     types = ['de.lad1337.anime']
-    _config = {'api_key': ''}
+    _config = {'enabled': True}
     config_meta = {'plugin_desc': 'Anime info from http://urani.me.'}
 
-    
+
+    def searchForElement(self, term='')
+        self.progress.reset()
+        
+        mt = MediaType.get(MediaType.identifier == 'de.lad1337.uranime')
+        mtm = mt.manager
+        rootElement = mtm.getFakeRoot(term)
+        payload = {}
+        url = 'http://api.urani.me/search'
+        payload['q'] = term
+        r = requests.get(url, params=payload)
+        
+        log('tgdb search url ' + r.url)
+        root = ET.fromstring(r.text.encode('utf-8'))
+
+        #baseImgUrlTag = root.find('baseImgUrl')
+        #if baseImgUrlTag is not None:
+        #    base_url = baseImgUrlTag.text
+        #else:
+        #    base_url = "http://thegamesdb.net/banners/"
+
+        #for curGame in root.getiterator('Game'):
+        #    self._createGameFromTag(curGame, base_url, rootElement)
+
+        log("%s found %s games" % (self.name, self.progress.count))
+
+        return rootElement
 }
