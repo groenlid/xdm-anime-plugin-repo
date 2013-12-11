1631ab74a45f5b96adee80cec8c1f6313edd613e
Mats Groenlid <groenlid@threepwood>
2013-12-11 17:11:47 +0100

    change namespace

--- Anime/Anime.py
+++ Anime/Anime.py
@@ -1,98 +1,49 @@
-# Author: Dennis Lutter <lad1337@gmail.com>
-# URL: https://github.com/lad1337/XDM
-#
-# This file is part of XDM: eXtentable Download Manager.
-#
-# XDM: eXtentable Download Manager. Plugin based media collection manager.
-# Copyright (C) 2013  Dennis Lutter
-#
-# XDM is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# XDM is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see http://www.gnu.org/licenses/.
 
 from xdm.plugins import *
 import os
 
 location = os.path.abspath(os.path.dirname(__file__))
 
-
 class Episode(object):
-    title = ''
-    airdate = 0
     number = 0
-    overview = ''
-    screencap_image = ''
-
+    title = ''
+    
     _orderBy = 'number'
-    _orderReverse = True
 
     def getTemplate(self):
-        fp = os.path.join(location, "episode.ji2")
-        with open (fp, "r") as template:
-            return template.read()
-
-    def getSearchTerms(self):
-        return ['%s s%02de%02d' % (self.parent.parent.title, self.parent.number, self.number)]
-
-    def getName(self):
-        return "%se%s %s" % (self.parent.getName(), self.number, self.title)
-
-    def getReleaseDate(self):
-        return self.airdate
-
-    def getIdentifier(self):
-        return self.number
+        return '<li class="bORw"><span class="songPosition bORw">{{this.number}}</span><span class="songTitle bORw">{{this.title}}</span><span class="length bORw">{{this.length}}</span></li>'
 
 class Show(object):
     title = ''
-    id = ''
-    airs = ''
-    overview = ''
-    year = 0
+    description = ''
     poster_image = ''
-    banner_image = ''
     fanart_image = ''
-    show_status = ''
-    genres = ''
-    runtime = ''
 
     _orderBy = 'title'
-    _orderReverse = True
-
-    def getTemplate(self):
-        fp = os.path.join(location, "show.ji2")
-        with open (fp, "r") as template:
-            return template.read()
-
-    def getSearchTemplate(self):
-        fp = os.path.join(location, "show_search.ji2")
-        with open (fp, "r") as template:
-            return template.read()
 
     def getName(self):
         return self.title
 
-    def getIdentifier(self):
-        return self.getField('id')
+    def getTemplate(self):
+	fp = os.path.join(location, "show.ji2")
+	with open (fp, "r") as template:
+		return template.read()
+
+    def getSearcTemplate(self):
+	fp = os.path.join(location, "show_search.ji2")
+	with open (fp, "r") as template:
+		return template.read()
+
+   
 
 class Anime(MediaTypeManager):
     version = "0.1"
     single = True
     _config = {'enabled': True}
-    config_meta = {'plugin_desc': 'Anime'}
+    config_meta = {'plugin_desc': 'Anime support'}
     order = (Show, Episode)
-    download = Episode
-    identifier = 'de.lad1337.anime'
-    xdm_version = (0, 5, 9)
+    download = Show
+    identifier = 'de.uranime.anime'
     addConfig = {}
     addConfig[Downloader] = [{'type':'category', 'default': None, 'prefix': 'Category for', 'sufix': 'Anime'}]
     addConfig[Indexer] = [{'type':'category', 'default': None, 'prefix': 'Category for', 'sufix': 'Anime'}]
@@ -103,41 +54,15 @@ class Anime(MediaTypeManager):
         show.status = common.getStatusByID(self.c.default_new_status_select)
         show.save()
         common.Q.put(('image.download', {'id': show.id}))
-        for episode in list(show.children):
-            episode.save()
-            common.Q.put(('image.download', {'id': episode.id}))
-        return True
+        #for season in list(show.children):
+        #    season.save()
+        #    common.Q.put(('image.download', {'id': season.id}))
+        #    for episode in list(season.children):
+        #        episode.save()
+        #        common.Q.put(('image.download', {'id': episode.id}))
+        return True    
+
 
     def headInject(self):
         return self._defaultHeadInject()
 
-    def homeStatuses(self):
-        return common.getEveryStatusBut([common.TEMP])
-
-    def getUpdateableElements(self, asList=True):
-        shows = Element.select().where(Element.type == 'Show',
-                                       Element.parent == self.root)
-        if asList:
-            return list(shows)
-        return shows
-
-    def _episode_count(self, show, statuses=False):
-        all_seasons = list(Element.select().where(Element.type == 'Season',
-                                              Element.parent == show))
-        seasons = [s for s in all_seasons if s.number]
-        if statuses:
-            return Element.select().where(Element.type == 'Episode',
-                                          Element.parent << seasons,
-                                          Element.status << statuses).count()
-        else:
-            return Element.select().where(Element.type == 'Episode',
-                                          Element.parent << seasons).count()
-
-    def _season_episode_count(self, season, statuses=False):
-        if statuses:
-            return Element.select().where(Element.type == 'Episode',
-                                          Element.parent == season,
-                                          Element.status << statuses).count()
-        else:
-            return Element.select().where(Element.type == 'Episode',
-                                          Element.parent == season).count()
--- Uranime/uranime.py
+++ Uranime/uranime.py
@@ -1,47 +1,63 @@
 from xdm.plugins import *
 import datetime
-import xml.etree.ElementTree as ET
 from lib import requests
+import json
 import re
 from lib.dateutil.parser import parser as dateParser
 
-import uranime
-
 class Uranime(Provider):
-    version = "0.1"
-    identifier = "de.lad1337.uranime"
+    version = "0.2"
+    identifier = "de.uranime.uranime"
     _tag = 'uranime'
     _additional_tags = ['anime']
     single = True
-    types = ['de.lad1337.anime']
+    types = ['de.uranime.anime']
     _config = {'enabled': True}
     config_meta = {'plugin_desc': 'Anime info from http://urani.me.'}
-
+    _resize_url = "http://urani.me/api/imageresize/"
 
     def searchForElement(self, term=''):
         self.progress.reset()
         
-        mt = MediaType.get(MediaType.identifier == 'de.lad1337.anime')
-        mtm = common.PM.getMediaTypeManager('de.lad1337.anime')[0]
+        mt = MediaType.get(MediaType.identifier == 'de.uranime.anime')
+        mtm = common.PM.getMediaTypeManager('de.uranime.anime')[0]
         rootElement = mtm.getFakeRoot(term)
         payload = {}
-        url = 'http://api.urani.me/search'
+        url = 'http://groenlid.no-ip.org/api/search'
         payload['q'] = term
         r = requests.get(url, params=payload)
         
-        log('tgdb search url ' + r.url)
-        root = ET.fromstring(r.text.encode('utf-8'))
+        log('uranime search url ' + r.url)
+        
+        searchresult = r.json()
+        self.progress.total = len(searchresult)
 
-        #baseImgUrlTag = root.find('baseImgUrl')
-        #if baseImgUrlTag is not None:
-        #    base_url = baseImgUrlTag.text
-        #else:
-        #    base_url = "http://thegamesdb.net/banners/"
+        # This is to support the old api
+        for item in searchresult:
+            self.progress.addItem()
+            log("found item: {}".format(item))
+            self._createAnime(rootElement, mt, item)
 
-        #for curGame in root.getiterator('Game'):
-        #    self._createGameFromTag(curGame, base_url, rootElement)
-
-        log("%s found %s games" % (self.name, self.progress.count))
+        log("%s found %s anime" % (self.name, self.progress.count))
 
         return rootElement
+    
+    def getElement(self, id, element=None):
+        log("GETELEMENT with id: {}".format(id))
+	mt = MediaType.get(MediaType.identifier == 'de.uranime.anime')
+	mtm = common.PM.getMediaTypeManager('de.uranime.anime')[0]
+	rootElement = mtm.getFakeRoot("{}{}".format(self._tag, id))
+		
+
+    def _createAnime(self, rootElement, mediaType, item):
+        showElement = Element()
+        showElement.mediaType = mediaType
+        showElement.parent = rootElement
+        showElement.type = 'Show'
+        showElement.setField('title', item['title'], self._tag)
+        showElement.setField('id', item['id'], self._tag)
+ 	showElement.setField('poster_image', self._resize_url + item['image'], self._tag)
+ 	showElement.setField('fanart_image', self._resize_url + item['fanart'], self._tag)
+   
+        showElement.saveTemp()
 
